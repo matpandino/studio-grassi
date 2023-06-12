10478991066b2747bf665554c2cba5635ccb405d
Mateus Pandino <matpandino@gmail.com>
2023-06-12 17:25:00 -0300

    feat: Add new product form with name, description, price and quantity inputs

--- src/app/admin/new/components/NewProductForm.tsx
+++ src/app/admin/new/components/NewProductForm.tsx
@@ -1,3 +1,4 @@
+import { Price, Input, TextArea } from "@/components/Inputs";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 
 export default function NewProductForm() {
@@ -7,85 +8,37 @@ export default function NewProductForm() {
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
           <div>
             <h2 className="text-base font-semibold leading-7 text-gray-900">
-              Profile
+              Novo Produto
             </h2>
             <p className="mt-1 text-sm leading-6 text-gray-600">
-              This information will be displayed publicly so be careful what you
-              share.
+              Criação de um novo produto
             </p>
           </div>
 
           <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
             <div className="sm:col-span-4">
-              <label
-                htmlFor="website"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Website
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input
-                    type="text"
-                    name="website"
-                    id="website"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
+              <Input id="name" name="name" labelContent="Nome" />
             </div>
 
             <div className="col-span-full">
-              <label
-                htmlFor="about"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Description
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="about"
-                  name="about"
-                  rows={3}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={""}
-                />
-              </div>
+              <TextArea
+                id="description"
+                name="description"
+                labelContent="Descrição"
+              />
             </div>
 
-            <div className="col-span-full">
-              <label
-                htmlFor="cover-photo"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Cover photo
-              </label>
-              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                <div className="text-center">
-                  <PhotoIcon
-                    className="mx-auto h-12 w-12 text-gray-300"
-                    aria-hidden="true"
-                  />
-                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                    >
-                      <span>Upload a file</span>
-                      <input
-                        id="file-upload"
-                        name="file-upload"
-                        type="file"
-                        className="sr-only"
-                      />
-                    </label>
-                    <p className="pl-1">or drag and drop</p>
-                  </div>
-                  <p className="text-xs leading-5 text-gray-600">
-                    PNG, JPG, GIF up to 10MB
-                  </p>
-                </div>
-              </div>
+            <div className="sm:col-span-3">
+              <Price id="price" name="price" labelContent="Preço" />
+            </div>
+
+            <div className="sm:col-span-3">
+              <Input
+                id="quantity"
+                name="quantity"
+                labelContent="Quantidade"
+                type="number"
+              />
             </div>
           </div>
         </div>
--- src/app/admin/new/page.tsx
+++ src/app/admin/new/page.tsx
@@ -1,5 +1,11 @@
 import NewProductForm from "./components/NewProductForm";
 
 export default function Page() {
-  return <NewProductForm />;
+  return (
+    <main className="flex w-full justify-center mt-10">
+      <div className="md:max-w-5xl md:flex-1">
+        <NewProductForm />
+      </div>
+    </main>
+  );
 }
